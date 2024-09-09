aff798dc81319342fa40f2f72b7899b00dd17f31
Ruan Correa <rhcs@ic.ufal.br>
2024-09-09 17:12:14 -0300

    feat: config vereadora mercia da saude

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header(props: HeaderProps) {
     let describe = "Com mais de 30 anos de experiência como técnica de enfermagem, dediquei minha vida a cuidar da saúde e bem-estar da nossa comunidade. Agora, estou pronta para levar esse compromisso ao próximo nível como candidata a Vereadora por Atalaia."
 
     return (
-        <header className="flex flex-col-reverse lg:flex-row lg:justify-between ${styles.header}">
+        <header className="flex flex-col lg:flex-row lg:justify-between ${styles.header}">
             {/*<InfoCard title={title} describe={describe}/>*/}
             <ImageCard path_image="/nome_numero.jpg" />
             <ImageCard path_image="/perfil_01.jpg" />
